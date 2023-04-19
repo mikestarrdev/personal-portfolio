efc9a647b8b9c1faf5b7572445a63d9090611a17
Mike Starr <imhs@protonmail.com>
2023-04-18 23:23:36 -0400

    setup react-hook-form

---
+++ public/hooks/useNodemailer.ts
@@ -0,0 +1,27 @@
+import { MailData } from "../../types/MailData";
+
+/**
+ * @remarks - hook gets called when a user requests funds from another user. Hook sends a request to the server to send an email to the user who is being requested funds from. Hook gets called in ReviewTransfer component after user confirms inputs.
+ * @param MailData - includes email, name, subject, message
+ * @returns hook with a fetch request to the server to send an email to the user who is being requested funds from.
+ */
+const UseNodemailer = async (data: MailData) => {
+  return await fetch("/api/auth/contact", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json, text/plain, */*",
+    },
+    body: JSON.stringify(data),
+  }).then((res) => {
+    console.log(res);
+    if (res.ok) {
+      console.log("response ok");
+      return res.json();
+    } else {
+      throw new Error("Failed to send message");
+    }
+  });
+};
+
+export default UseNodemailer;
